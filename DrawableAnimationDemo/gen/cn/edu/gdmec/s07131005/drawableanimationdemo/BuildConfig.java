d8799719d46bb6b99cdf5ab84db7c8e5a9b15e0b
gdmec07131005 <420718954@qq.com>
2014-11-28 11:35:47 +0800

    DrawableAnimation

---
+++ DrawableAnimationDemo/gen/cn/edu/gdmec/s07131005/drawableanimationdemo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cn.edu.gdmec.s07131005.drawableanimationdemo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
